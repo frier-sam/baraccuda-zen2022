b1c597f1fc7fc608a95505555b7092cd9783757a
Sam <uumasai@zenon.ai>
2023-02-11 13:07:08 +0530

    updates

--- src/views/uploadview.js
+++ src/views/uploadview.js
@@ -6,6 +6,15 @@ import { useHistory } from 'react-router-dom';
 import {baseURL} from '../api/apis';
 // react-bootstrap components
 // import {Form} from "react-bootstrap";
+import Card from 'react-bootstrap/Card';
+import { BsUpload,BsBezier2,BsFillCloudUploadFill,BsFillFileEarmarkRuledFill,BsSliders } from "react-icons/bs"
+import {
+
+  Row,
+  Col
+
+} from "react-bootstrap";
+
 
 function UploadView() {
   // const [imageURL, setimageURL] = useState('');
@@ -34,17 +43,82 @@ function UploadView() {
  
   return (
     <>
-      {/* <Form.Group controlId="formFile" className="mb-3" onSubmit={onSubmit}>
-        <Form.Label>Choose a File</Form.Label>
-        <Form.Control type="file"  onChange={onChange} />
-        <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      </Form.Group> */}
-      <form onSubmit={onSubmit}>
-      <input type="file" onChange={onChange} />
-      <button type="submit">Upload</button>
-    </form>
+      <Row>
+        <Col size={4}>
+          <Card>
+            <div className="text-center mt-1"><BsUpload  size={70} /></div>
+            <Card.Body>
+              <Card.Title className="text-center"><strong>Upload</strong></Card.Title>
+              <hr/>
+              <form onSubmit={onSubmit}>
+                <input type="file" onChange={onChange} />
+                <hr/>
+                <div className="text-center">
+                  {/* <button type="submit">Upload</button> */}
+                  <Button variant="primary" type="submit">Upload</Button>
+                </div>
+              </form>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col size={4}>
+          <Card>
+            <div className="text-center mt-1"><BsBezier2  size={70} /></div>
+            <Card.Body>
+              <Card.Title className="text-center"><strong>Database Connect</strong></Card.Title>
+              <hr/>
+              <Card.Text>
+                Connect with any relational database to start the analysis process
+              </Card.Text>
+              <div className="text-center"><Button variant="primary">Connect</Button></div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col size={4}>
+          <Card>
+            <div className="text-center  mt-1"><BsFillCloudUploadFill  size={70} /></div>
+            <Card.Body>
+              <Card.Title className="text-center"><strong>Cloud Connect</strong></Card.Title>
+              <hr/>
+              <Card.Text>
+                Connect with clound instances for logs and timeseries data.
+              </Card.Text>
+              <div className="text-center"><Button variant="primary">Connect</Button></div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col size={4}>
+          <Card>
+            <div className="text-center mt-2"><BsFillFileEarmarkRuledFill  size={70} /></div>
+            <Card.Body>
+              <Card.Title className="text-center"><strong>Sales Force</strong></Card.Title>
+              <hr/>
+              <Card.Text>
+                Connect with sales force
+              </Card.Text>
+              <div className="text-center"><Button variant="primary">Connect</Button></div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col size={6}>
+          <Card>
+            <div className="text-center"><BsSliders  size={70} /></div>
+            <Card.Body>
+              <Card.Title className="text-center"><strong>Api Integration</strong></Card.Title>
+              <hr/>
+              <Card.Text>
+                Connect with any custome api for live data view.
+              </Card.Text>
+              <div className="text-center"><Button variant="primary">Connect</Button></div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      
+      
+      
     </>
   );
 }
